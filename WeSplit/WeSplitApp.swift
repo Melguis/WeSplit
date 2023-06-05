0a264466e3c71143d3a023d6fe4aad66fd6afbad
Jorge Henrique <jorgehenriquedasilva2014@hotmail.com>
2023-06-05 19:00:59 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jorge Henrique on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
